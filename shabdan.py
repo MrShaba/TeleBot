9e2a1149d74d5feffdb622ed920b45e7856276bc
MrShaba <b8mvtbvxvp@privaterelay.appleid.com>
2025-12-22 18:52:38 +0600

    Add files via upload

---
+++ shabdan.py
@@ -0,0 +1,11 @@
+print("Baktybek uulu Shabdan")
+float=("07.08.1998,rost 1.86")
+str="Python", 'Java'
+bool=True, False
+print("Hello, world!") 
+
+print  ("Shabdan"), "{name}"("27"), {age}, "'(status", (1, 86)
+
+str="Shabdan","{name}"("27"), {age}, "'(status", (1, 86)
+
+# jdjccdcdcdfffffff
